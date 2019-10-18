8ac3e3b86f82d06b4ed58a7034addaf28335d183
KimDoYeon <dykim0224@ajou.ac.kr>
2019-10-18 15:55:26 +0900

    idea페이지 값 넘겨주기, 화면전환

--- D_AI_Project/D_AI_Service/views.py
+++ D_AI_Project/D_AI_Service/views.py
@@ -4,23 +4,26 @@ def home(request):
     return render(request, 'home.html')
 
 def idea(request):
-    return render(request, 'idea.html')
-
-def idea_create(request):
     if request.method == "POST":
         keyword1 = request.POST["keyword1"]
         keyword2 = request.POST["keyword2"]
         association = request.POST["association"]
         key_num = request.POST["key_num"]
-        # if key_num is not None:
-        #     auth.login(request, user)
-        #     return render(request, 'idea.html')
-        # else:
-        return render(request, 'idea.html')
+        # N_list 만드는 코드 작성
+        N_list = ['당근', '사과', '포도', '딸기', '아주대', '소웨', '혁중', '지석', '도연', '2조','당근', '사과', '포도', '딸기', '아주대', '소웨', '혁중', '지석', '도연', '2조','당근', '사과', '포도', '딸기', '아주대', '소웨', '혁중', '지석', '도연', '2조']
+        return render(request, 'idea.html', {'N_list': N_list})
+    else:
+        # 랜덤 키워드 10개 뽑아오는 코드 작성
+        keyword = ['당근', '사과', '포도', '딸기', '아주대', '소웨', '혁중', '지석', '도연', '2조']
+        return render(request, 'idea.html', {'keyword': keyword})
+
+def ideaResult(request):
+    if request.method == "POST":
+        sel_N = request.POST["select_N"]
+        # print(sel_N)
+        return render(request, 'ideaResult.html', {'N': sel_N})
     else:
         return render(request, 'idea.html')
-    return render(request, 'idea.html')
-
 def mindmap(request):
     return render(request, 'mindmap.html')
 
